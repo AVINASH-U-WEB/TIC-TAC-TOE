fafef3fddc10752981e1dbb93425ac993a7a2a4d
Avinash <112330806+Uavina@users.noreply.github.com>
2024-11-05 16:54:17 +0530

    tic

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TicTacToe from './Compoentes/Tic-Tac-Toe/TicTacToe';
+
+function App() {
+  return (
+    <div>
+      <TicTacToe />
+    </div>
+  );
+}
+
+export default App;
